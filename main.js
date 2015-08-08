a5f580e7375d14148c1d6ed18660817dca0aeed2
Greg Dick <gregory.j.dick@vanderbilt.edu>
2015-08-08 13:33:06 -0500

    styling and got rid of extra dependencies

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 var API_URL = 'http://api.wunderground.com/api/1a83bcf806654e1a/geolookup/forecast/q/';
+var where = document.querySelector('.where');
 var forecast = document.querySelector('.forecast');
 var button = document.querySelector('input[type = button]');
 
@@ -11,23 +12,25 @@ function getZipTemp(){
   getJSON(API_URL+ zip +'.json', change);
 }
 
-function change(data){  
-  var htmltext='';
-  htmltext+= data.location.city +', '+data.location.state+'\n';
+function change(data){
+  var whereText = '';
+  var htmltext= '';
+  whereText += data.location.city + ', ' + data.location.state;
 	htmltext += data.forecast.txt_forecast.forecastday[0].fcttext;
+  where.innerHTML = whereText;
   forecast.innerHTML = htmltext;
 }
 
 function getJSON(url, cb){
   var xhr = new XMLHttpRequest();
-  
+
   xhr.open('GET', url);
   xhr.onload = function(){
     if(this.status >= 200 && this.status < 400){
       cb(JSON.parse(this.response));
     }
   };
-  
+
   xhr.send();
 }
 
